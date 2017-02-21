b99b4282f01ca16e75dbb290562ee5f8d22c9a22
Stefan Thöni <stefan.thoeni@piatenpartei.ch>
2017-02-21 21:25:35 +0100

    Updated docker

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
 FROM ubuntu:16.04
 
+RUN echo "A1"
 RUN apt-get update
 RUN apt-get upgrade -y
 RUN apt-get install -y python-pip python-dev build-essential git libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
+RUN pip install --upgrade pip
 RUN git clone https://github.com/tallus/python-civicrm
 RUN cd python-civicrm && python setup.py install
+RUN echo "B5"
 RUN git clone https://github.com/edemocracy/ekklesia
 RUN cd ekklesia && make install
 RUN git clone https://github.com/ppschweiz/python-civi
--- run.sh
+++ run.sh
@@ -24,28 +24,52 @@ then
   cd /
 fi
 
-echo "Exporting members..."
-touch /data/memberlist
-cd /python-civi && python export_members.py /data/memberlist > /tmp/members.csv
+while :
+do
+	echo "Exporting members..."
+	touch /data/memberlist
+	cd /python-civi && python export_members.py /data/memberlist > /tmp/members.csv
 
-echo "Departments members..."
-cd /python-civi && python export_departments.py > /tmp/departments.csv
+	echo "Departments members..."
+	cd /python-civi && python export_departments.py > /tmp/departments.csv
 
-echo "Importing members and departments..."
-env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /data/ekklesia.ini import -s /tmp/members.csv /tmp/departments.csv
+	echo "Importing members and departments..."
+	env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /data/ekklesia.ini -v import -s /tmp/members.csv /tmp/departments.csv
 
-echo "Exporting members for transfer..."
-env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /data/ekklesia.ini export /tmp/transfer.csv
+	echo "Exporting members for transfer..."
+	env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /data/ekklesia.ini -v export /tmp/transfer.csv
 
-echo "Importing invitations..."
-env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /data/ekklesia.ini import -s /tmp/transfer.csv
+	echo "Importing invitations..."
+	env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /data/ekklesia.ini -v import -s /tmp/transfer.csv
+	
+	echo "Resetting invitations..."
+	env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /data/ekklesia.ini -v reset -u /data/reset
+	rm /data/reset
+	touch /data/reset
 
-echo "Syncing invitations..."
-env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /data/ekklesia.ini sync
+	echo "Syncing invitations..."
+	env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /data/ekklesia.ini -v sync
 
-echo "Syncing members..."
-env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /data/ekklesia.ini sync
+	echo "Syncing members..."
+	env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /data/ekklesia.ini -v sync
 
-echo "Sending invitations..."
-env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /data/ekklesia.ini send
+	echo "Sending invitations..."
+	env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /data/ekklesia.ini -v send
+	
+	echo "Syncing invitations..."
+	env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /data/ekklesia.ini -v sync
 
+	echo "Syncing members..."
+	env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /data/ekklesia.ini -v sync
+
+	echo "Round finished. Waiting 5 minutes..."
+	sleep 60
+	echo "Waiting 4 minutes..."
+	sleep 60
+	echo "Waiting 3 minutes..."
+	sleep 60
+	echo "Waiting 2 minutes..."
+	sleep 60
+	echo "Waiting 1 minutes..."
+	sleep 60
+done
