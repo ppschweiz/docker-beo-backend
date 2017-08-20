8d5e5cf7adac40bc69c93083beb4921a9d8426c1
Jan Fader <jan.fader@web.de>
2017-08-20 20:46:25 +0200

    centralized initialisation of static files

--- push.sh
+++ push.sh
@@ -5,5 +5,5 @@ source static.sh
 echo "Backend starting..."
 
 echo "Waiting for rabbitmq..."
-env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /data/ekklesia.ini -v push
+env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /ekklesia.ini -v push
 
