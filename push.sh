f8c991f2378783379b5632b17570a1add342a8e8
Stefan Thoeni <stefan.thoeni@piratenpartei.ch>
2017-08-20 18:13:19 +0200

    Removed old sqlite

--- push.sh
+++ push.sh
@@ -2,28 +2,6 @@
 
 echo "Backend starting..."
 
-if [ ! -f "/data/ekklesia.ini" ]
-then
-  echo "ekklesia.ini not found; copying template..."
-  cp /ekklesia.ini.template /data/ekklesia.ini
-fi
-
-if [ ! -f "/data/members.sqlite" ]
-then
-  echo "members.sqlite not found; initializing..."
-  cd /data
-  env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /data/ekklesia.ini init
-  cd /
-fi
-
-if [ ! -f "/data/invitations.sqlite" ]
-then
-  echo "invitations.sqlite not found; initializing..."
-  cd /data
-  env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /data/ekklesia.ini init
-  cd /
-fi
-
 echo "Waiting for rabbitmq..."
 env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /data/ekklesia.ini -v push
 
--- run.sh
+++ run.sh
@@ -10,31 +10,15 @@ then
   cp /ekklesia.ini.template /data/ekklesia.ini
 fi
 
-if [ ! -f "/data/members.sqlite" ]
-then
-  echo "members.sqlite not found; initializing..."
-  cd /data
-  env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /data/ekklesia.ini init
-  cd /
-fi
-
-if [ ! -f "/data/invitations.sqlite" ]
-then
-  echo "invitations.sqlite not found; initializing..."
-  cd /data
-  env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /data/ekklesia.ini init
-  cd /
-fi
-
 while :
 do
 	echo "Exporting members..."
-	touch /data/memberlist
-	cd /python-civi && python3 export_members.py /data/reset /tmp/gpg > /tmp/members.csv
+	touch /tmp/memberlist
+	cd /python-civi && python3 export_members.py /tmp/reset /tmp/gpg > /tmp/members.csv
 
 	echo "Import gpg keys..."
-	gpg --homedir /data/gnupg/ --import /tmp/gpg/*
-	gpg --homedir /data/gnupg/ --refresh-keys --keyserver pgp.mit.edu
+	gpg --import /tmp/gpg/*
+	gpg --refresh-keys --keyserver pgp.mit.edu
 
 	echo "Departments members..."
 	cd /python-civi && python3 export_departments.py > /tmp/departments.csv
@@ -49,9 +33,9 @@ do
 	env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /data/ekklesia.ini -v import -s /tmp/transfer.csv
 	
 	echo "Resetting invitations..."
-	env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /data/ekklesia.ini -v reset -u /data/reset
-	rm /data/reset
-	touch /data/reset
+	env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /data/ekklesia.ini -v reset -u /tmp/reset
+	rm /tmp/reset
+	touch /tmp/reset
 
 	echo "Syncing invitations..."
 	env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /data/ekklesia.ini -v sync
