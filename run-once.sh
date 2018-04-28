f39882656d4b21d9e48364a9990418bcfeba5636
Stefan Thöni <stefan@savvy.ch>
2018-04-28 19:57:04 +0200

    Aborting on bad exit codes

---
+++ run-once.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+#while true; do echo "..."; sleep 10; done
+
+source static.sh
+
+echo "Exporting members..."
+touch /tmp/memberlist
+cd /python-civi && python3 export_members.py /tmp/reset /tmp/gpg > /tmp/members.csv
+if [ $? -ne 0 ma]
+then
+	echo "Export members from civi FAILED"
+	exit 1
+fi
+
+echo "Import gpg keys..."
+gpg --import /tmp/gpg/*
+gpg --refresh-keys --keyserver pgp.mit.edu
+
+echo "Departments members..."
+cd /python-civi && python3 export_departments.py > /tmp/departments.csv
+if [ $? -ne 0 ma]
+then
+	echo "Export departments from civi FAILED"
+	exit 2
+fi
+
+echo "Importing members and departments..."
+env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /ekklesia.ini -v import -s /tmp/members.csv /tmp/departments.csv
+if [ $? -ne 0 ma]
+then
+	echo "Import members into backend FAILED"
+	exit 3
+fi
+
+echo "Exporting members for transfer..."
+env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /ekklesia.ini -v export /tmp/transfer.csv
+if [ $? -ne 0 ma]
+then
+	echo "Export members from backend FAILED"
+	exit 4
+fi
+
+echo "Importing invitations..."
+env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /ekklesia.ini -v import -s /tmp/transfer.csv
+if [ $? -ne 0 ma]
+then
+	echo "Import invitations into backend FAILED"
+	exit 5
+fi
+	
+echo "Resetting invitations..."
+env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /ekklesia.ini -v reset -u /tmp/reset
+if [ $? -ne 0 ma]
+then
+	echo "Resetting members on backend FAILED"
+	exit 6
+fi
+rm /tmp/reset
+touch /tmp/reset
+
+echo "Syncing invitations..."
+env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /ekklesia.ini -v sync
+if [ $? -ne 0 ma]
+then
+	echo "Syncing invitations with backend FAILED"
+	exit 7
+fi
+
+echo "Syncing members..."
+env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /ekklesia.ini -v sync
+if [ $? -ne 0 ma]
+then
+	echo "Syncing members with backend FAILED"
+	exit 8
+fi
+
+echo "Sending invitations..."
+env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /ekklesia.ini -v send
+if [ $? -ne 0 ma]
+then
+	echo "Sending invitations with backend FAILED"
+	exit 9
+fi
+	
+echo "Syncing invitations..."
+env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /ekklesia.ini -v sync
+if [ $? -ne 0 ma]
+then
+	echo "Syncing invitations with backend FAILED"
+	exit 10
+fi
+
+echo "Syncing members..."
+env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /ekklesia.ini -v sync
+if [ $? -ne 0 ma]
+then
+	echo "Syncing members with backend FAILED"
+	exit 11
+fi
+
--- run.sh
+++ run.sh
@@ -6,46 +6,11 @@ source static.sh
 
 while :
 do
-	echo "Exporting members..."
-	touch /tmp/memberlist
-	cd /python-civi && python3 export_members.py /tmp/reset /tmp/gpg > /tmp/members.csv
+	date
 
-	echo "Import gpg keys..."
-	gpg --import /tmp/gpg/*
-	gpg --refresh-keys --keyserver pgp.mit.edu
+	/run-once.sh
 
-	echo "Departments members..."
-	cd /python-civi && python3 export_departments.py > /tmp/departments.csv
-
-	echo "Importing members and departments..."
-	env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /ekklesia.ini -v import -s /tmp/members.csv /tmp/departments.csv
-
-	echo "Exporting members for transfer..."
-	env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /ekklesia.ini -v export /tmp/transfer.csv
-
-	echo "Importing invitations..."
-	env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /ekklesia.ini -v import -s /tmp/transfer.csv
-	
-	echo "Resetting invitations..."
-	env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /ekklesia.ini -v reset -u /tmp/reset
-	rm /tmp/reset
-	touch /tmp/reset
-
-	echo "Syncing invitations..."
-	env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /ekklesia.ini -v sync
-
-	echo "Syncing members..."
-	env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /ekklesia.ini -v sync
-
-	echo "Sending invitations..."
-	env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /ekklesia.ini -v send
-	
-	echo "Syncing invitations..."
-	env PYTHONPATH=/ekklesia python -m ekklesia.backends.invitations -C /ekklesia.ini -v sync
-
-	echo "Syncing members..."
-	env PYTHONPATH=/ekklesia python -m ekklesia.backends.members -C /ekklesia.ini -v sync
-
+	date
 	echo "Round finished. Waiting 15 minutes..."
 	sleep 300
 	echo "Waiting 10 minutes..."
